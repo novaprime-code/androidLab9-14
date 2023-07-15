05684d9b67a3564ad3294dde61d5e2fc5df122ef
Jay Shyam Patel <jayshyampatel133@gmail.com>
2023-07-16 03:51:58 +0545

    lab 9 to 14

---
+++ app/src/main/java/com/example/lab9to14/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.lab9to14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnExp9, btnExp10, btnExp11, btnExp12, btnExp13,btnExp14;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnExp9 = findViewById(R.id.btnExperiment9);
+        btnExp10 = findViewById(R.id.btnExperiment10);
+        btnExp11 = findViewById(R.id.btnExperiment11);
+        btnExp12 = findViewById(R.id.btnExperiment12);
+        btnExp13 = findViewById(R.id.btnExperiment13);
+        btnExp14 = findViewById(R.id.btnExperiment14);
+
+        btnExp9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Experiment9Activity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
